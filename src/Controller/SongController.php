73986fe361610df55d62ddd6f960ced8fef420b7
Eya soyed <eyaso@LAPTOP-NK71BM2P.swayedaya@gmail.com>
2024-03-12 08:50:33 +0100

    last music list

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -11,12 +11,41 @@ class SongController extends AbstractController
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
     public function getSong($id): Response
     {
-        // TODO query the database
-        $song = [
-            'id' => $id,
-            'name' => 'Waterfalls',
-            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        $songs = [
+            1=>[
+            'id' => 1,
+            'name' => 'Kangaroo_MusiQue',
+            'url' => 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+            ],
+            2=>[
+            'id' => 2,
+            'name' => 'Sevish',
+            'url' => 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+            ],
+            3=>[
+                'id' => 3,
+                'name' => 'Evillaugh',
+                'url' => 'https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg',
+            ],
+            4=>[
+                'id' => 4,
+                'name' => 'pazamodules',
+                'url' => 'https://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3',
+            ],
+            5=>[
+                'id' => 5,
+                'name' => 'race1',
+                'url' => 'https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/race1.ogg',
+            ],
+            6=>[
+                'id' => 6,
+                'name' => 'win',
+                'url' => 'https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/win.ogg',
+            ],
+                
+            
         ];
-        return $this->json($song);
+        
+        return $this->json($songs[$id]);
     }
 }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -10,12 +10,12 @@ class VinylController extends AbstractController{
     #[Route("/",name:'app_homepage')]
     function homePage():Response{
         $tracks = [
-            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
-            ['song' => 'Waterfalls', 'artist' => 'TLC'],
-            ['song' => 'Creep', 'artist' => 'Radiohead'],
-            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+            ['song' => 'Kangaroo_MusiQue', 'artist' => 'Kangaro'],
+            ['song' => 'Sevish', 'artist' => 'SS'],
+            ['song' => 'Evillaugh', 'artist' => 'soundeffects'],
+            ['song' => 'pazamodules', 'artist' => 'pang'],
+            ['song' => 'race', 'artist' => 'riceracer'],
+            ['song' => 'win', 'artist' => 'riceracer'],
         ];
        
         return $this->render('vinyl/homepage.html.twig',[
